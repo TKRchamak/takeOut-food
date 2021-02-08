1c13e02ce4c5a3acc4d9cc979020bb596037655c
Tonmoy Kumar Roy <tkrchamak9813467@gmail.com>
2021-02-08 13:03:52 +0600

    modal

--- app.js
+++ app.js
@@ -1,101 +1,67 @@
-// API call
-const button = document.getElementById('searchButton');
-const mealPlace = document.getElementById('meal');
-const resultPlace= document.getElementById('showResult');
+const searchButton = () => {
+    const foodName = document.getElementById("inputFood").value;
 
-button.addEventListener('click', (event) => {
-    event.preventDefault();
-    const userInput = document.getElementById('inputForSearch').value;
-    loadData(userInput);
-});
 
-function loadData(userInput) {
+    // let url = "";
+    // if (userInput.length === 1) {
+    //     url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`;
+    //     mealPlace.innerHTML = null;
+    //     resultPlace.innerHTML = null;
 
-    let url = "";
-    if (userInput.length === 1) {
-        url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${userInput}`;
-        mealPlace.innerHTML = null;
-        resultPlace.innerHTML = null;
+    // } else {
+    //     url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`;
+    //     mealPlace.innerHTML = null;
+    //     resultPlace.innerHTML = null;
+    // }
 
-    } else {
-        url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`;
-        mealPlace.innerHTML = null;
-        resultPlace.innerHTML = null;
-    }
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => {
-            displayData(data)
-        })
-    //.catch(error => console.log(error))
+        .then(data => displayFoods(data.meals))
 }
+const displayFoods = foods => {
+    const showFood = document.getElementById('displayFoods');
+    foods.forEach(food => {
+        console.log(food);
+        const foodDiv = document.createElement('div');
+        // div.className = 'row row-cols-2 row-cols-md-4 g-4 p-4';
+        foodDiv.innerHTML = `
+        <div data-bs-toggle="modal" data-bs-target="#staticBackdrop" class="card" style="width: 18rem;">
+            <img src="${food.strMealThumb}" class="card-img-top" alt="nothing">
+            <div class="card-body">
+                <h5 class="card-title">${food.strMeal}</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+    `;
+        showFood.appendChild(foodDiv);
+        const foodDetail = document.getElementById('staticBackdrop');
+        foodDetail.innerHTML = `
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <img src="${food.strMealThumb}" class="card-img-top" alt="nothing">
+                </div>
+                <div class="modal-body">
+                    <h5 class="modal-title" id="staticBackdropLabel">${food.strMeal}</h5>
+                    <h3></h3>
+                    <ul>
+                        <li><i class="material-icons"></i>${food.strIngredient1}</li>
+                        <li><i class="material-icons"></i>${food.strIngredient2}</li>
+                        <li><i class="material-icons"></i>${food.strIngredient3}</li>
+                        <li><i class="material-icons"></i>${food.strIngredient4}</li>
+                        <li><i class="material-icons"></i>${food.strIngredient5}</li>
+                        <li><i class="material-icons"></i>${food.strIngredient6}</li>
+                        <li><i class="material-icons"></i>${food.strIngredient7}</li>
 
-
-
-const displayData = data => {
-
-    data.meals.forEach(meal => {
-
-        const mealDiv = document.createElement('div');
-
-        const mealIntro = `
-        <div  class="col">
-            <div class="card h-100">
-                <img  class="card-img-top" src="${meal.strMealThumb}"/>
-                <div class="card-body">
-                    <h5 class="card-title mx-auto">${meal.strMeal}</h5>
-                    
+                    </ul>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 </div>
             </div>
-        </div> `;
-        mealDiv.innerHTML = mealIntro;
-        mealPlace.appendChild(mealDiv);
-
+        </div>
+        `;
     });
-}
 
-const displayMealDetails = (string) => {
-
-    url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${string}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-
-            console.log('JUST STRING NAME : ', string);
-
-            resultPlace.style.display = "block"
-            const div = document.createElement('div');
-
-            let meal;
-            let mealInfo;
-            for (let i = 0; i < data.meals.length; i++) {
-                meal = data.meals[i];
-
-                if (string === meal.strMeal) {
-                    mealInfo = `
-                    <img src="${meal.strMealThumb}" class="card-img-top">
-                    <div class="card-body">
-                    <h3 class="card-title">${meal.strMeal}</h3>
-                    <p>Ingredients</p>
-                    <ul>                    
-                        <li>${meal.strIngredient1}</li>
-                        <li>${meal.strIngredient2}</li>
-                        <li>${meal.strIngredient3}</li>
-                        <li>${meal.strIngredient4}</li>
-                        <li>${meal.strIngredient5}</li>
-                        <li>${meal.strIngredient6}</li>
-                        <li>${meal.strIngredient7}</li>
-                        <li>${meal.strIngredient8}</li>
-                        <li>${meal.strIngredient9}</li>
-                        <li>${meal.strIngredient10}</li>
-                    </ul>
-                    </div> `;
-                }
-            }
-
-            mealDiv.innerHTML = mealInfo;
-            resultPlace.appendChild(mealDiv);
-        });
-    
-    resultPlace.innerHTML = null;
 }
